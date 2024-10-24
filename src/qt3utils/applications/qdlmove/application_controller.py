e9da9cff4043aab97c8c7030aa489175abe5b527
nyama8 <nyama8@hawaii.edu>
2024-10-23 17:48:03 -0700

    Updated comments and documentation.

--- src/qt3utils/applications/qdlmove/application_controller.py
+++ src/qt3utils/applications/qdlmove/application_controller.py
@@ -8,11 +8,18 @@ logging.basicConfig(level=logging.INFO)
 
 
 class MovementController:
-
+    '''
+    This is a basic application controller backend for the `qdlmove` application.
+    In its current implementation, it manages calls to move or step the various 
+    hardware by reference to their name as stored in the `self.positioners` dict.
+    
+    In the future, if more features are desired of the `qdlmove` application, 
+    for example recording/saving of positions, plotting points, etc. the developer
+    should add these features into this class and update the GUI/main application
+    to trigger methods or store data in this class.
+    '''
     def __init__(self, positioners: dict = {}):
-
         self.positioners = positioners # dictionary of the controller instantiations
-        self.step_sizes = {}
 
 
     def move_axis(self, axis_controller_name: str, position: float):
